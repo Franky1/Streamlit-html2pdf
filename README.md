536158a6fa9144f8512faa75b036f65dcd836f4b
Arvid Teichtmann <arvid.teichtmann@gmx.de>
2021-04-19 16:34:37 +0200

    first running version

--- README.md
+++ README.md
@@ -20,3 +20,40 @@ Some links to articles and different libraries for html2pdf conversion in Python
 - <https://gearheart.io/blog/how-generate-pdf-files-python-xhtml2pdf-weasyprint-or-unoconv/>
 - <https://elements.heroku.com/buildpacks/jazzcore/python-pdfkit>
 - <https://github.com/JazzCore/python-pdfkit>
+
+## Docker Runtime
+
+The provided `Dockerfile` tries to mimic the Streamlit Sharing runtime.
+
+Pulling the base image from Docker Hub
+
+```shell
+docker pull python:3.7.10-slim
+```
+
+Run and shell into base python container
+
+```shell
+docker run -it --name py3710slim python:3.7.10-slim /bin/bash
+docker run -ti --rm python:3.7.10-slim /bin/bash
+```
+
+Build local custom Docker Image from Dockerfile
+
+```shell
+docker build -t html2pdf:latest .
+docker run -ti --name selenium --rm html2pdf:latest /bin/bash
+```
+
+Run custom Docker Container
+
+```shell
+docker run -ti -p 8501:8501 --rm html2pdf:latest
+docker run -ti -p 8501:8501 -v $(pwd):/app --rm html2pdf:latest  # linux
+docker run -ti -p 8501:8501 -v ${pwd}:/app --rm html2pdf:latest  # powershell
+docker run -ti -p 8501:8501 -v %cd%:/app --rm html2pdf:latest  # cmd.exe
+```
+
+Open the local Streamlit application
+
+<http://localhost:8501> for the local or dockerized application
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,43 @@
+import base64
+
 import pdfkit
 import streamlit as st
 
 
-def html2pdf():
-    pdfkit.from_url('https://www.google.de/', 'google.pdf')
+def html2pdf(url, pdf_file):
+    pdfkit.from_url(url, pdf_file)
+
+
+def readpdf(pdf_file):
+    with open(pdf_file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    return pdf_display
 
 
 if __name__ == "__main__":
-    st.set_page_config(page_title="Selenium Test", page_icon='✅',
+    st.set_page_config(page_title="HTML2PDF", page_icon='✅',
         initial_sidebar_state='collapsed')
-    st.title('🔨 Selenium Test for Streamlit Sharing')
+    st.title('🔨 HTML2PDF Test for Streamlit Sharing')
     st.markdown("""
-        This app is only a very simple test for **Selenium** running on **Streamlit Sharing** runtime. <br>
+        This app is only a very simple test for html2pdf on **Streamlit Sharing** runtime. <br>
         The suggestion for this demo app came from a post on the Streamlit Community Forum.  <br>
         <https://discuss.streamlit.io/t/deploying-an-app-using-using-wkhtmltopdf-on-herokou/12029>  <br>
 
         This is just a very very simple example and a proof of concept.
         A link is called and converted HTML to PDF.
         Afterwards the pdf file is displayed.
-        ---
+
+        The downside is that websites with content created by javascript will probably not be rendered properly.
+        Onyl static website content will be downloaded and therefore rendered to pdf.
         """, unsafe_allow_html=True)
     st.balloons()
-    if st.button('Start Selenium run'):
-        st.info('Selenium is running, please wait...')
-        html2pdf()
-        st.info('Successful finished.')
+    if st.button('Start html2pdf run'):
+        url = "https://discuss.streamlit.io/"
+        st.info(f'Download and render pdf from url: {url}')
+        st.info('html2pdf is running, please wait...')
+        pdf_file = "download.pdf"
+        html2pdf(url=url, pdf_file=pdf_file)
+        pdf_display = readpdf(pdf_file)
+        st.info('Successful finished. See PDF below.')
+        st.markdown(pdf_display, unsafe_allow_html=True)
